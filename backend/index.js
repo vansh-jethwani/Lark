3bfe97baddce7e4ac8b787deeaf10084103748a1
Vansh Jethwani <vanshjethwani896@gmail.com>
2026-06-23 09:30:55 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
