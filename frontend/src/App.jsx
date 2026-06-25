45b38a1dcadce0b66f3795bb08db6bcd067be63f
Vansh Jethwani <vanshjethwani896@gmail.com>
2026-06-26 00:49:51 +0530

    implement authentication page layout and frontend infrastructure with Clerk and HeroUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,13 @@ import { Routes, Route, Navigate } from 'react-router';
 import ChatPage from './pages/chatPage';
 import AuthPage from './pages/AuthPage';
 import { useAuth } from '@clerk/react';
+import PageLoader from './components/PageLoader';
 
 function App() {
 
   const { isSignedIn, isLoaded } = useAuth();
 
-  if(!isLoaded) return <div>Loading...</div>;
+  if(!isLoaded) return <PageLoader />;
 
   return (
     <ThemeProvider>
